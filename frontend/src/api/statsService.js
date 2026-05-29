b4dea16e1b812f53a71edb527497e8516c339cdc
nifers464-boop <nifers464@gmail.com>
2026-05-29 12:02:40 +0800

    Delete frontend directory

--- frontend/src/api/statsService.js
+++
@@ -1,8 +0,0 @@
-import apiClient from './apiClient';
-
-export const statsService = {
-  getStats: async () => {
-    const response = await apiClient.get('/stats');
-    return response.data;
-  }
-};
